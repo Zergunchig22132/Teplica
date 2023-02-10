13becfaf3187c67714ac4fb3d8d27e66f5770c06
Zergunchig22132 <56889544+Zergunchig22132@users.noreply.github.com>
2023-02-10 12:12:37 +0300

    Create making maket.py

---
+++ making maket.py
@@ -0,0 +1,12 @@
+from kivy.uix.floatlayout import FloatLayout
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.carousel import Carousel
+from kivy.uix.colorpicker import ColorPicker
+from kivy.uix.image import Image
+from kivy.uix.textinput import TextInput
+from kivy.uix.modalview import ModalView
+from kivy.uix.spinner import Spinner
+from kivy.uix.switch import Switch
+from kivy.properties import NumericProperty, StringProperty, ListProperty, DictProperty, ObjectProperty, BooleanProperty, BoundedNumericProperty
